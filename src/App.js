f6eb674f8bf07b73e826766c90fbec94bc594635
mauro Leos <mauroleos94@gmail.com>
2021-01-16 01:48:32 -0600

    Added images, fonts, and Header

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+        {/*items => snapping */}
+        {/*items => snapping */}
+        {/*items => snapping */}
+        {/*items => snapping */}
+    </div>
+  );
+}
+
+export default App;
